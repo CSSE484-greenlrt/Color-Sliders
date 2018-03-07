0caf508f3d2e352f9e08262791d7b58cdd1744fe
Ryan Greenlee <student@ryans-macbook.local>
2018-03-07 12:11:13 -0500

    initial commit

--- Color Sliders/ViewController.swift
+++ Color Sliders/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var alphaSlider: UISlider!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var alphaLabel: UILabel!
+    
+    @IBOutlet weak var colorView: UIView!
+
+    @IBAction func sliderChanged(_ sender: AnyObject) {
+        print("slider changed")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
